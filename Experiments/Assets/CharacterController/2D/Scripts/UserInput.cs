3cd29830a1535f91cfddeb437eb28d2ea1333f79
Oscar Losada <oscarlosu@gmail.com>
2017-02-14 23:53:28 +0100

    Added SVGImporter. Setup 2DCharacterController base.

---
+++ Experiments/Assets/CharacterController/2D/Scripts/UserInput.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserInput : MonoBehaviour {
+    [SerializeField]
+    [Range(-1.0f, 1.0f)]
+    protected float xInput;
+    public float XInput {
+        get {
+            return xInput;
+        }
+        set {
+            xInput = Mathf.Clamp(value, 0.0f, 1.0f);
+        }
+    }
+    [SerializeField]
+    [Range(-1.0f, 1.0f)]
+    protected float yInput;
+    public float YInput {
+        get {
+            return yInput;
+        }
+        set {
+            yInput = Mathf.Clamp(value, 0.0f, 1.0f);
+        }
+    }
+
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
