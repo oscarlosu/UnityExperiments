097c267937574f8c20aebd95af2d7339b6699467
Oscar Losada <oscarlosu@gmail.com>
2017-09-20 00:19:27 +0200

    removed svg importer small phone gyro and accelerometer tests

---
+++ Experiments/Assets/PhoneMotion/MonitorPhoneMotion.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonitorPhoneMotion : MonoBehaviour {
+    public Vector3 acceleration;
+    public Vector3 velocity;
+    public bool accelerometer = false;
+
+    public Vector3 angularVelocity;
+    int touchCount;
+
+    // Use this for initialization
+    void Start () {
+        Input.gyro.enabled = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        touchCount = Input.touchCount;
+        if(touchCount == 2 && Input.GetTouch(touchCount - 1).phase == TouchPhase.Began)
+        {
+            accelerometer = !accelerometer;
+        }
+
+        // Rotation (from https://forum.unity.com/threads/unity-and-the-accelerometer-vs-the-gyroscope-a-complete-guide.451496/)
+        transform.rotation = Input.gyro.attitude;
+        transform.Rotate(0f, 0f, 180f, Space.Self); // Swap "handedness" of quaternion from gyro.
+        transform.Rotate(90f, 180f, 0f, Space.World); // Rotate to make sense as a camera pointing out the back of your device
+
+        // Linear velocity
+        acceleration = transform.TransformDirection(Input.gyro.userAcceleration);
+        velocity += acceleration * Time.deltaTime;
+        if (accelerometer)
+        {
+            transform.position += velocity * Time.deltaTime;
+        }
+        
+
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(new Vector2(0, 0), new Vector2(100, 50)), new GUIContent("Acceleration " + acceleration.ToString()));
+        GUI.Label(new Rect(new Vector2(0, 50), new Vector2(150, 50)), new GUIContent("Velocity " + velocity.ToString()));
+        GUI.Label(new Rect(new Vector2(0, 100), new Vector2(150, 50)), new GUIContent("Touches " + touchCount.ToString()));
+        GUI.Label(new Rect(new Vector2(0, 150), new Vector2(150, 50)), new GUIContent("Acceleration " + accelerometer));
+    }
+}
---
+++ Experiments/Assets/ViewOrthoOutline/OrthoViewOutline.shader
@@ -0,0 +1,57 @@
+﻿// Upgrade NOTE: replaced '_Object2World' with 'unity_ObjectToWorld'
+
+// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Custom/OrthoViewOutline" {
+	Properties {		
+		_Color ("Color", Color) = (0.0, 0.0, 0.0, 1.0)
+		_OutlineColor ("Outline Color", Color) = (1.0, 1.0, 1.0, 1.0)
+		_Thickness ("Thickness", float) = 0.1
+	}
+	SubShader {
+		Pass {
+			CGPROGRAM
+			// pragmas
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+
+			// user defined variables
+			uniform float4 _Color;
+			uniform float4 _OutlineColor;
+			uniform float _Thickness;
+			// base input structs
+			struct vertexInput {
+				float4 vertex : POSITION;
+				float3 normal : NORMAL;
+			};
+			struct vertexOutput {
+				float4 pos : SV_POSITION;
+				float3 normal : NORMAL;
+				float3 viewDir : TEXCOORD0;
+			};
+			// vertex function
+			vertexOutput vert(vertexInput i) {
+				vertexOutput o;
+				o.pos = UnityObjectToClipPos(i.vertex);
+				o.normal = i.normal;
+				o.viewDir = mul((float3x3)unity_ObjectToWorld, i.vertex) - _WorldSpaceCameraPos;
+				return o;
+			}
+			// fragment function
+			float4 frag(vertexOutput i) : COLOR {
+				float d = dot(i.normal, i.viewDir);
+				//return abs(d) * _OutlineColor;
+				if(abs(d) < _Thickness) {
+					return _OutlineColor;
+				}
+
+				return _Color;
+			}
+			ENDCG
+		}
+	}
+	// fallback commented out during developments
+	//Fallback "Diffuse"
+}
