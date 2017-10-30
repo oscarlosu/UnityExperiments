79b11b681f6eadf2830bc07f7d47bd7665b01975
Oscar Losada <oscarlosu@gmail.com>
2017-10-30 23:16:35 +0100

    water foam

---
+++ Experiments/Assets/Water/EnableDepthTexture.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableDepthTexture : MonoBehaviour {
+    public Camera camera;
+	// Use this for initialization
+	void Start () {
+        camera.depthTextureMode = DepthTextureMode.Depth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
